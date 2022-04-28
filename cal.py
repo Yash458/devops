e9eab1c5340ed34073c22c6ffb8655225a8f2eb0
Yash458 <yashpahuja785@gmail.com>
2022-04-28 19:26:04 +0530

    add cal

---
+++ cal.py
@@ -0,0 +1,9 @@
+import sys 
+a = 5
+
+b = 4
+
+print ("Addition",a+b)
+print ("Subtraction", a-b)
+print ("Multiplication of ", a ,"and", b ,"is", a*b)
+print ("Division of ", a ,"and", b, "is", a/b)
